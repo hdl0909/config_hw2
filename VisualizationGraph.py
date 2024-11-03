9e973e5bea4b8fdd239031f908d14fe0ea70a4c9
Данил <da_nil2005@mail.ru>
2024-11-03 19:46:24 +0300

    Исправление рекурсивного обхода

--- VisualizationGraph.py
+++ VisualizationGraph.py
@@ -1,28 +1,25 @@
 import requests
 import os
-import sys
 
 def input_package_info():
-    if len(sys.argv) < 4:
-        print("Использование: python <path_to_script> <package_name> <result_file_path> <max_depth>")
-        sys.exit(1)
+    package_name = input("Введите имя анализируемого пакета: ").lower()
+    max_depth = int(input("Введите максимальную глубину анализа зависимостей: "))
+    result_file_path = input("Введите путь для записи кода PlantUML: ")
+    return package_name, max_depth, result_file_path 
 
-    package_name = sys.argv[1]
-    result_file_path = sys.argv[2]
-    max_depth = int(sys.argv[3])
-    return package_name, max_depth, result_file_path
-
-def fetch_package_dependencies(package_name, max_depth, current_depth=0):
+def fetch_package_dependencies(package_name):
+    # URL для получения метаданных пакета
     url = f"https://api.nuget.org/v3/registration5-semver1/{package_name}/index.json"
     response = requests.get(url)
-
+  
+    dependencies = []
+    
     if response.status_code == 200:
         data = response.json()
-        dependencies = []
 
         # Проход по группам зависимостей
         for root in data['items']:
-            for groups in root['items']:
+            for groups in root.get('items', []):
                 for group in groups['catalogEntry'].get('dependencyGroups', []):
                     if 'dependencies' in group:
                         for dependency in group['dependencies']:
@@ -30,37 +27,43 @@ def fetch_package_dependencies(package_name, max_depth, current_depth=0):
                                 'id': dependency['id'],
                                 'range': dependency['range']
                             })
-
-        # Если текущая глубина меньше максимальной, рекурсивно ищем зависимости
-        '''if current_depth < max_depth:
-            for dep in dependencies:
-                # Рекурсивно извлекаем зависимости для каждой найденной
-                sub_dependencies = fetch_package_dependencies(dep['id'], max_depth, current_depth + 1)
-                dependencies.extend(sub_dependencies)'''
-
     return dependencies
 
-def generate_plantuml(dependencies):
-    """Генерирует код PlantUML для визуализации зависимостей."""
-    plantuml_code = "@startuml\n"
+def build_dependency_graph(package_name, max_depth, current_depth=0, graph=None):
+    if graph is None:
+        graph = []
+    
+    if current_depth >= max_depth:
+        return graph
+
+    # Получаем зависимости текущего пакета
+    dependencies = fetch_package_dependencies(package_name)
     
     for dep in dependencies:
-        plantuml_code += f"'{dep['id']}' --> '{dep['range']}'\n"
-    
-    plantuml_code += "@enduml"
+        # Добавляем связь в граф
+        graph.append(f"'{package_name}' --> '{dep['id']}'")
+        # Рекурсивно получаем зависимости для подзависимостей
+        build_dependency_graph(dep['id'], max_depth, current_depth + 1, graph)
+
+    return graph
+
+def generate_plantuml(dependency_graph):
+    """Генерирует код PlantUML для визуализации зависимостей."""
+    plantuml_code = "@startuml\n"
+    plantuml_code += "\n".join(dependency_graph)
+    plantuml_code += "\n@enduml"
     return plantuml_code
 
 def main():
     # Получаем информацию о пакете от пользователя
     package_name, max_depth, result_file_path = input_package_info()
 
-    # Извлекаем зависимости из API NuGet
-    dependencies = fetch_package_dependencies(package_name, max_depth)
+    # Генерация графа зависимостей
+    dependency_graph = build_dependency_graph(package_name, max_depth)
 
     # Генерация кода PlantUML
-    plantuml_code = generate_plantuml(dependencies)
+    plantuml_code = generate_plantuml(dependency_graph)
 
-    # Запись кода в файл
     with open(result_file_path, 'w') as f:
         f.write(plantuml_code)
 
