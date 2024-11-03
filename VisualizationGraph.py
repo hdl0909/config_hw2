269b46cba53a04ac6892810d71e6a126399bbf3e
Данил <da_nil2005@mail.ru>
2024-11-03 19:18:36 +0300

    на вход принимает аргументы

--- VisualizationGraph.py
+++ VisualizationGraph.py
@@ -1,62 +1,42 @@
-import argparse
 import requests
+import os
+import sys
 
-def get_package_info_and_dependencies(package_name, repository_url):
-    """Получает информацию о пакете, его версиях и зависимостях из NuGet."""
-    # Получаем информацию о пакете
-    package_info_url = f"{repository_url}/registration5-semver1/{package_name}/index.json"
-    response = requests.get(package_info_url)
+def input_package_info():
+    if len(sys.argv) < 4:
+        print("Использование: python <path_to_script> <package_name> <result_file_path> <max_depth>")
+        sys.exit(1)
 
-    if response.status_code != 200:
-        print(f"Ошибка при получении данных для пакета {package_name}: {response.status_code}")
-        return None
+    package_name = sys.argv[1]
+    result_file_path = sys.argv[2]
+    max_depth = int(sys.argv[3])
+    return package_name, max_depth, result_file_path
 
-    package_info = response.json()
-    dependencies = {}
+def fetch_package_dependencies(package_name, max_depth, current_depth=0):
+    url = f"https://api.nuget.org/v3/registration5-semver1/{package_name}/index.json"
+    response = requests.get(url)
 
-    # Для каждой версии получаем зависимости
-    for item in package_info['items']:
-        for version in item['items']:
-            version_number = version['catalogEntry']['version']
-            dep_url = version['catalogEntry']  # URL для получения дополнительных данных о пакете
-            
-            # Здесь нужно использовать сам URL из catalogEntry, который вы получили в предыдущем запросе
-            dep_response = requests.get(version['catalogEntry']['@id'])
+    if response.status_code == 200:
+        data = response.json()
+        dependencies = []
 
-            if dep_response.status_code == 200:
-                # Проверяем, что ответ является JSON
-                try:
-                    dep_data = dep_response.json()
-                except ValueError:
-                    print(f"Ошибка: ответ не является корректным JSON для версии {version_number}.")
-                    continue
-                
-                deps = dep_data.get('dependencies', [])
-                dependencies[version_number] = deps
-            else:
-                print(f"Ошибка при получении зависимостей для версии {version_number}: {dep_response.status_code}")
+        # Проход по группам зависимостей
+        for root in data['items']:
+            for groups in root['items']:
+                for group in groups['catalogEntry'].get('dependencyGroups', []):
+                    if 'dependencies' in group:
+                        for dependency in group['dependencies']:
+                            dependencies.append({
+                                'id': dependency['id'],
+                                'range': dependency['range']
+                            })
 
-    return package_info, dependencies
-
-
-
-
-def recursive_dependencies(package_name, version, repository_url, depth):
-    """Рекурсивно получает все зависимости с учетом глубины."""
-    if depth < 0:
-        return {}
-
-    dependencies = {}
-    deps = get_package_info_and_dependencies(package_name, repository_url)[1].get(version, [])
-    
-    dependencies[version] = deps
-
-    for dep in deps:
-        dep_name = dep['id']
-        dep_version = dep['version']
-        if dep_name not in dependencies:
-            nested_deps = recursive_dependencies(dep_name, dep_version, repository_url, depth - 1)
-            dependencies.update(nested_deps)
+        # Если текущая глубина меньше максимальной, рекурсивно ищем зависимости
+        '''if current_depth < max_depth:
+            for dep in dependencies:
+                # Рекурсивно извлекаем зависимости для каждой найденной
+                sub_dependencies = fetch_package_dependencies(dep['id'], max_depth, current_depth + 1)
+                dependencies.extend(sub_dependencies)'''
 
     return dependencies
 
@@ -64,57 +44,25 @@ def generate_plantuml(dependencies):
     """Генерирует код PlantUML для визуализации зависимостей."""
     plantuml_code = "@startuml\n"
     
-    for version, deps in dependencies.items():
-        for dep in deps:
-            plantuml_code += f"'{version}' --> '{dep['id']} {dep['version']}'\n"
+    for dep in dependencies:
+        plantuml_code += f"'{dep['id']}' --> '{dep['range']}'\n"
     
     plantuml_code += "@enduml"
     return plantuml_code
 
 def main():
-    ''' parser = argparse.ArgumentParser(description="Визуализатор зависимостей .NET пакета в формате PlantUML")
-    parser.add_argument('--path', required=True, help='Путь к программе для визуализации графов')
-    parser.add_argument('--package', required=True, help='Имя анализируемого пакета')
-    parser.add_argument('--output', required=True, help='Путь к файлу-результату в виде кода')
-    parser.add_argument('--depth', type=int, required=True, help='Максимальная глубина анализа зависимостей')
-    parser.add_argument('--repo-url', required=True, help='URL-адрес репозитория')
+    # Получаем информацию о пакете от пользователя
+    package_name, max_depth, result_file_path = input_package_info()
 
-    args = parser.parse_args()
+    # Извлекаем зависимости из API NuGet
+    dependencies = fetch_package_dependencies(package_name, max_depth)
 
-    package_info, dependencies = get_package_info_and_dependencies(args.package, args.repo_url) '''
-    
-    path = input("Введите путь к программе для визуализации графов: ")
-    package = input("Введите имя анализируемого пакета: ")
-    output = input("Введите путь к файлу-результату в виде кода: ")
-    depth = int(input("Введите максимальную глубину анализа зависимостей: "))
-    repo_url = input("Введите URL-адрес репозитория: ")
+    # Генерация кода PlantUML
+    plantuml_code = generate_plantuml(dependencies)
 
-    package_info, dependencies = get_package_info_and_dependencies(package, repo_url)
-    
-    if not package_info:
-        return
-
-    # Получаем зависимости для всех версий пакета
-    all_dependencies = {}
-    for version in dependencies:
-        #dep_data = recursive_dependencies(args.package, version, args.repo_url, args.depth)
-        dep_data = recursive_dependencies(package, version, repo_url, depth)
-        all_dependencies.update(dep_data)
-        
-    if all_dependencies:
-        plantuml_code = generate_plantuml(all_dependencies)
-        
-        # Выводим код на экран
-        print(plantuml_code)
-
-        # Сохраняем в файл
-        #with open(args.output, 'w') as file:
-        with open(output, 'w') as file:
-            file.write(plantuml_code)
-        #print(f"Код PlantUML сохранен в {args.output}")
-        print(f"Код PlantUML сохранен в {output}")
-    else:
-        print("Зависимости не найдены.")
+    # Запись кода в файл
+    with open(result_file_path, 'w') as f:
+        f.write(plantuml_code)
 
 if __name__ == "__main__":
     main()
