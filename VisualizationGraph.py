35eb877e7369ecedb5482baab2fa54f6db846417
hdl0909 <135420447+hdl0909@users.noreply.github.com>
2024-11-11 10:09:11 +0300

    добавление сообщения в сгенерированном коде PlantUML

--- VisualizationGraph.py
+++ VisualizationGraph.py
@@ -1,5 +1,4 @@
 import requests
-import os
 
 def input_package_info():
     package_name = input("Введите имя анализируемого пакета: ").lower()
@@ -8,7 +7,6 @@ def input_package_info():
     return package_name, max_depth, result_file_path 
 
 def fetch_package_dependencies(package_name):
-    # URL для получения метаданных пакета
     url = f"https://api.nuget.org/v3/registration5-semver1/{package_name}/index.json"
     response = requests.get(url)
   
@@ -17,7 +15,6 @@ def fetch_package_dependencies(package_name):
     if response.status_code == 200:
         data = response.json()
 
-        # Проход по группам зависимостей
         for root in data['items']:
             for groups in root.get('items', []):
                 for group in groups['catalogEntry'].get('dependencyGroups', []):
@@ -29,39 +26,39 @@ def fetch_package_dependencies(package_name):
                             })
     return dependencies
 
-def build_dependency_graph(package_name, max_depth, current_depth=0, graph=None):
+def build_dependency_graph(package_name, max_depth, current_depth=0, graph=None, visited=None):
     if graph is None:
         graph = []
+    if visited is None:
+        visited = set()
     
     if current_depth >= max_depth:
         return graph
 
-    # Получаем зависимости текущего пакета
     dependencies = fetch_package_dependencies(package_name)
     
     for dep in dependencies:
-        # Добавляем связь в граф
-        graph.append(f"'{package_name}' --> '{dep['id']}'")
-        # Рекурсивно получаем зависимости для подзависимостей
-        build_dependency_graph(dep['id'], max_depth, current_depth + 1, graph)
+        # Создаем строку для проверки и добавления в граф
+        dependency_str = f"\"{package_name}\" -> \"{dep['id']}\" : \"{dep['range']}\""
+        
+        # Проверяем, если зависимость уже была добавлена, чтобы избежать дублирования
+        if dependency_str not in visited:
+            graph.append(dependency_str)
+            visited.add(dependency_str)
+            # Рекурсивно обрабатываем зависимости
+            build_dependency_graph(dep['id'], max_depth, current_depth + 1, graph, visited)
 
     return graph
 
 def generate_plantuml(dependency_graph):
-    """Генерирует код PlantUML для визуализации зависимостей."""
     plantuml_code = "@startuml\n"
     plantuml_code += "\n".join(dependency_graph)
     plantuml_code += "\n@enduml"
     return plantuml_code
 
 def main():
-    # Получаем информацию о пакете от пользователя
     package_name, max_depth, result_file_path = input_package_info()
-
-    # Генерация графа зависимостей
     dependency_graph = build_dependency_graph(package_name, max_depth)
-
-    # Генерация кода PlantUML
     plantuml_code = generate_plantuml(dependency_graph)
 
     with open(result_file_path, 'w') as f:
